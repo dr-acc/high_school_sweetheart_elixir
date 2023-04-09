0914ac616d4dc7216ad944e2d6b554dd31e8f1c0
Your Name <you@example.com>
2023-04-09 12:04:02 -0700

    cleaned up with mentoring tips from Jimmar

--- lib/high_school_sweetheart.ex
+++ lib/high_school_sweetheart.ex
@@ -16,12 +16,12 @@ defmodule HighSchoolSweetheart do
     names = String.split(full_name)
     first = List.first(names)
     last = List.last(names)
-    "#{__MODULE__.initial(first)} #{__MODULE__.initial(last)}"
+    "#{initial(first)} #{initial(last)}"
   end
 
   def pair(full_name1, full_name2) do
-    i1 = __MODULE__.initials(full_name1)
-    i2 = __MODULE__.initials(full_name2)
+    i1 = initials(full_name1)
+    i2 = initials(full_name2)
     """
          ******       ******
        **      **   **      **
